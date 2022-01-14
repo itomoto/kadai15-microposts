9631e967cac0ff69cd32d88960e96423fe6ff8b4
motohiro.ito <uthitt.1216.i.m@gmail.com>
2022-01-14 23:04:40 +0000

    btnmodify

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
 {{--@if (Auth::id() != $user->id)--}}
     @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete'])!!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-light btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
 {{--@endif--}}
